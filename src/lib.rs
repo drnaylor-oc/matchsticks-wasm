0c941be61c2362c33e7a83bbd64578def88e0818
Daniel Naylor <daniel.naylor@opencastsoftware.com>
2024-11-05 11:49:28 +0000

    Remove some repetition

--- src/lib.rs
+++ src/lib.rs
@@ -27,16 +27,8 @@ struct ErrorResult<'a> {
 }
 
 #[wasm_bindgen]
-pub fn calculate_largest(number_of_matchsticks: usize, result: String) -> Result<JsValue, JsValue> {
-    calculate_largest_internal(number_of_matchsticks)
-        .map(|x| {
-            to_json_result(
-                &OkResult {
-                    correct: x.result.eq(result.trim())
-                }
-            ).unwrap()
-        })
-        .map_err(|x| to_json_result(&x).unwrap())
+pub fn calculate_largest(number_of_matchsticks: usize, input: String) -> Result<JsValue, JsValue> {
+    translate_result(calculate_largest_internal(number_of_matchsticks), &input)
 }
 
 fn calculate_largest_internal(number_of_matchsticks: usize) -> Result<CalcResult, ErrorResult<'static>> {
@@ -55,16 +47,8 @@ fn calculate_largest_internal(number_of_matchsticks: usize) -> Result<CalcResult
 }
 
 #[wasm_bindgen]
-pub fn calculate_smallest(number_of_matchsticks: usize, result: String) -> Result<JsValue, JsValue> {
-    calculate_smallest_internal(number_of_matchsticks)
-        .map(|x| {
-            to_json_result(
-                &OkResult {
-                    correct: x.result.eq(result.trim())
-                }
-            ).unwrap()
-        })
-        .map_err(|x| to_json_result(&x).unwrap())
+pub fn calculate_smallest(number_of_matchsticks: usize, input: String) -> Result<JsValue, JsValue> {
+    translate_result(calculate_smallest_internal(number_of_matchsticks), &input)
 }
 
 fn calculate_smallest_internal(number_of_matchsticks: usize) -> Result<CalcResult, ErrorResult<'static>> {
@@ -90,16 +74,8 @@ fn calculate_smallest_internal(number_of_matchsticks: usize) -> Result<CalcResul
 }
 
 #[wasm_bindgen]
-pub fn calculate_smallest_no_leading_zeroes(number_of_matchsticks: usize, result: String) -> Result<JsValue, JsValue> {
-    calculate_smallest_no_leading_zeroes_internal(number_of_matchsticks)
-        .map(|x| {
-            to_json_result(
-                &OkResult {
-                    correct: x.result.eq(result.trim())
-                }
-            ).unwrap()
-        })
-        .map_err(|x| to_json_result(&x).unwrap())
+pub fn calculate_smallest_no_leading_zeroes(number_of_matchsticks: usize, input: String) -> Result<JsValue, JsValue> {
+    translate_result(calculate_smallest_no_leading_zeroes_internal(number_of_matchsticks), &input)
 }
 
 //noinspection RsNonExhaustiveMatch
@@ -162,6 +138,15 @@ fn get_digits_and_remainder(number_of_matchsticks: &usize, division: usize) -> (
     (number_of_matchsticks / division, number_of_matchsticks % division)
 }
 
+fn translate_result(result: Result<CalcResult, ErrorResult>, input: &String) -> Result<JsValue, JsValue> {
+    result.map(|x| {
+        to_json_result(
+            &OkResult {
+                correct: x.result.eq(input.trim())
+            }
+        ).unwrap()
+    }).map_err(|x| to_json_result(&x).unwrap())
+}
 
 // Run these tests using one of the following:
 //
